f032d99d95456fa22681e865359242ffbe2796c9
Mike McCourt <mkmcc@astro.berkeley.edu>
2017-01-09 14:48:54 -0800

    initial commit

---
+++ check.el
@@ -0,0 +1,90 @@
+;; dossier.el -- organizes academic applications
+;;
+(require 's)
+(require 'dash)
+
+;; file processing stuff
+
+(defun get-files (patt)
+  "use the find command to get a list of files matching the glob pattern PATT"
+  (let ((str (shell-command-to-string (concat "find . -name '" patt  "'"))))
+    (-filter 's-present? (s-split "\n" str))))
+
+(defun file-to-list (file)
+  "read FILE in as a list of lines"
+  (with-temp-buffer
+    (insert-file-contents file)
+    (split-string (buffer-string) "\n" t)))
+
+(defun get-all-files (keyfile)
+  "given the KEYFILE, return a list of files in it in order"
+  (let ((dir (s-chop-suffix "/files.lst" keyfile))
+        (lst (file-to-list keyfile)))
+    (--map (concat dir "/" it) lst)))
+
+(defun my-delete (filename)
+  (when (file-exists-p filename)
+    (delete-file filename)))
+
+
+;; LaTeX stuff
+
+(defun build-latex-file (filename files)
+  "makes a LaTeX file which uses pdfpages to combine all of the files
+in the list FILES.  each file opens on an odd page.  output is written
+to FILENAME."
+  (with-temp-file filename
+    (insert "\\batchmode
+\\documentclass[twoside]{article}
+\\usepackage[letterpaper]{geometry}
+\\usepackage[utf8]{inputenc}
+\\usepackage{hyperref}
+\\usepackage{pdfpages}
+\\pagestyle{empty}
+
+\\begin{document}" "\n" "\\tableofcontents\n\n")
+
+    (--map (insert "\\cleardoublepage\n\\phantomsection\n"
+                   "\\addcontentsline{toc}{section}{" it "}\n"
+                   "\\includepdf[pages={1-}]{" it "}" "\n\n")
+           files)
+
+    (insert "\\end{document}" "\n")))
+
+(defun run-latex-file (filename)
+  (call-process "rubber" nil nil nil "-df" filename))
+
+(defun make-latex-file (basename)
+  (let ((filename (concat basename ".tex")))
+    (run-latex-file filename)
+    (run-latex-file filename)
+
+    (my-delete (concat basename ".tex"))
+    (my-delete (concat basename ".toc"))
+    (my-delete (concat basename ".out"))
+    (my-delete (concat basename ".log"))
+    (my-delete (concat basename ".aux"))))
+
+
+;; make a dossier for a single applicant
+
+(defun make-dossier (keyfile)
+  (let* ((basename (s-chop-suffix "/files.lst" (s-chop-prefix "./" keyfile)))
+         (filename (concat basename ".tex")))
+
+    (build-latex-file filename (get-all-files keyfile))
+    (make-latex-file basename)
+
+    (concat basename ".pdf")))
+
+
+;; make all the dossiers and assemble into a master file
+
+(let ((dossiers (-map 'make-dossier (get-files "files.lst"))))
+  (build-latex-file "applications.tex" dossiers)
+  (make-latex-file "applications")
+
+  (message "finished!"))
+
+
+;; fin
